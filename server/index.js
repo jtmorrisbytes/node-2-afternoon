84d446e22c8ea9b95759bcb87cfc091c0a9cd501
Jordan Morris <jthecybertinkerer@gmail.com>
2020-03-15 08:30:21 -0500

    Autocommit 2/0/2020 8:30:21.639 CST: Jordan Morris (jthecybertinkerer@gmail.com) updated file at 'server/index.js'

--- server/index.js
+++ server/index.js
@@ -16,7 +16,12 @@ app.post("/api/messages", (req, res) => {
   }
   //   res.status(501).json({ error: "not implemented" });
 });
-
+app.put("/api/messages", (req, res) => {
+  if (!req.body.id) {
+    res.status(409).json({ error: 409 });
+  }
+  res.json(messageController.updateMessage());
+});
 app.listen(3001, () => {
   console.log("api server listening on port 3001");
 });
