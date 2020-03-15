374e5aefd64d9647cf161a216789d85e83dda713
Jordan Morris <jthecybertinkerer@gmail.com>
2020-03-15 07:41:48 -0500

    Autocommit 2/0/2020 7:41:48.902 CST: Jordan Morris (jthecybertinkerer@gmail.com) updated file at 'server/controllers/message_controller.js'

--- server/controllers/message_controller.js
+++ server/controllers/message_controller.js
@@ -2,11 +2,14 @@ let messages = [];
 let id = 0;
 
 const messageModel = {
+  id: 0,
   text: "",
   date: ""
 };
 
-createMessage = (text, date) => {};
+createMessage = (text, date) => {
+  messages.push({ ...messageModel, text, date });
+};
 getMessages = () => {
   return messages;
 };
