c0ad4e88761b695a9c99fb7215c7606e93243195
Jordan Morris <jthecybertinkerer@gmail.com>
2020-03-15 07:32:21 -0500

    Autocommit 2/0/2020 7:32:20.991 CST: Jordan Morris (jthecybertinkerer@gmail.com) updated file at 'server/index.js'

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/api/messages", (req, res) => {});
+app.get("/api/messages", (req, res) => {
+  res.sendStatus(20);
+});
 
 app.listen(3001, () => {
   console.log("api server listening on port 3001");
